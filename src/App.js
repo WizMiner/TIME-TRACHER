e87daabc0cbf12f779bb4b8be504c81e21c92ffe
Minte <mintesinottamene0917@gmail.com>
2024-11-15 15:01:15 +0300

    create task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import Login from "./pages/Login";
+import CreateTask from "./pages/CreateTask";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/CreateTask" element={<CreateTask />} />
       </Routes>
       <Footer />
     </BrowserRouter>
